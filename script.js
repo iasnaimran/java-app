85685e5e93076eeca4c9847617360c199f2144ce
iasnaimran <iasnaimran@gmail.com>
2025-10-31 12:11:18 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+    alert(" Hello world ....! ")
+
+    alert("welcome to js land....\n Happy coding!")
+
+    alert("Error! please enter a valid password ")
+
+    alert("Happy Coding!\n prevent this page form creating additional dialogs. ")
+   
+    alert("Hello...! I can run JS through my web browser's console.")
+
+
+
+
+
+
+
+
+
